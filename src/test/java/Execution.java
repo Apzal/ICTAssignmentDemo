90d5d6c78a19ab58aa089cdf8466d981a32ac0fb
Apzal Bahin <apzal.bn@gmail.com>
2020-09-10 01:38:48 +0530

    added demo code

--- src/test/java/Execution.java
+++ src/test/java/Execution.java
@@ -25,7 +25,7 @@ public class Execution {
 
         reports.attachReporter(extentSparkReporter);
         extentTest=reports.createTest("LoginTest");
-        WebDriver driver=Driver.openBrowser("chrome","https://opensource-demo.orangehrmlive.com/");
+        WebDriver driver=Driver.openBrowser("ie","https://opensource-demo.orangehrmlive.com/");
         Login loginPage=new Login(driver);
         loginPage.Login("admin","admin123");
         extentTest.log(Status.PASS,"Login Passed");
